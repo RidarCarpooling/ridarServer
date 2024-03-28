5f460e8846d8dbd94a1777c9ca19758962707cf9
heyounihao <97096673+heyounihao@users.noreply.github.com>
2024-03-28 21:11:25 +0800

    new

--- ecpayServer/payment/views.py
+++ ecpayServer/payment/views.py
@@ -4,6 +4,6 @@ from .testEcpay import main
 
 @csrf_exempt
 def index(request):
-    print(request.META)
+    print(request.headers)
     totalAmount = request.GET.get('totalPrice')
     return HttpResponse(main(totalAmount))
