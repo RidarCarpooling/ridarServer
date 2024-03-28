d321a70570c112d3ee0d1b1dda9060e796bc85f5
heyounihao <97096673+heyounihao@users.noreply.github.com>
2024-03-28 15:05:25 +0800

    new

--- ecpayServer/ecpayServer/wsgi.py
+++ ecpayServer/ecpayServer/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecpayServer.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ecpayServer.settings')
+# application = get_wsgi_application()
 
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
 application = get_wsgi_application()
+app = application
