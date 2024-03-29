75c4079294e7648b7ed08e5a7617373da7ef671d
heyounihao <97096673+heyounihao@users.noreply.github.com>
2024-03-29 11:24:59 +0800

    new

--- ecpayServer/ecpayServer/urls.py
+++ ecpayServer/ecpayServer/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('return/', include("receive_result.urls")),
+    path('return', include("receive_result.urls")),
     path('payment/', include("payment.urls")),
     path('admin/', admin.site.urls)
 ]
