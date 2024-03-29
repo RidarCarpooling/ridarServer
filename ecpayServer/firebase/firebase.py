cc89c7751dc28cb03f3416b174d1955a5c7b02c0
heyounihao <97096673+heyounihao@users.noreply.github.com>
2024-03-29 22:03:24 +0800

    new

--- ecpayServer/firebase/firebase.py
+++ ecpayServer/firebase/firebase.py
@@ -1,25 +1,23 @@
-from firebase_admin import firestore, initialize_app
-import os
-# os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "ridar-e1rlvk-firebase-adminsdk-31um4-04d2182145.json"
+from firebase_admin import firestore
 
-def initialize_firebase():
-    """
-    Initialize Firebase Admin SDK using environment variables.
-    """
-    firebase_credentials = {
-        "type": "service_account",
-        "project_id": os.environ.get("PROJECT_ID"),
-        "private_key_id": os.environ.get("PRIVATE_KEY_ID"),
-        "private_key": os.environ.get("PRIVATE_KEY").replace('\\n', '\n'),  # Replace escaped newlines
-        "client_email": os.environ.get("CLIENT_EMAIL"),
-        "client_id": os.environ.get("CLIENT_ID"),
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/tokenn",
-        "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-        "client_x509_cert_url": os.environ.get("CLIENT_CERT_URL")
-    }
+# def initialize_firebase():
+#     """
+#     Initialize Firebase Admin SDK using environment variables.
+#     """
+#     firebase_credentials = {
+#         "type": "service_account",
+#         "project_id": os.environ.get("PROJECT_ID"),
+#         "private_key_id": os.environ.get("PRIVATE_KEY_ID"),
+#         "private_key": os.environ.get("PRIVATE_KEY").replace('\\n', '\n'),  # Replace escaped newlines
+#         "client_email": os.environ.get("CLIENT_EMAIL"),
+#         "client_id": os.environ.get("CLIENT_ID"),
+#         "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+#         "token_uri": "https://oauth2.googleapis.com/tokenn",
+#         "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+#         "client_x509_cert_url": os.environ.get("CLIENT_CERT_URL")
+#     }
 
-    initialize_app(credential=firebase_credentials)
+#     initialize_app(credential=firebase_credentials)
 
 # initialize_app()
 
@@ -45,6 +43,3 @@ def write_to_firebase(transaction_id, transaction_data):
     db = firestore.client()
     db.collection('transactions').document(transaction_id).set(transaction_data)
     return transaction_id
-
-    
-initialize_firebase()
