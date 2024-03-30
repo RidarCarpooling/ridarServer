0ad1508933e1516b93d4d95d1243824c925a2118
heyounihao <97096673+heyounihao@users.noreply.github.com>
2024-03-30 14:45:07 +0800

    new

--- ecpayServer/receive_result/views.py
+++ ecpayServer/receive_result/views.py
@@ -18,10 +18,12 @@ def receive_payment_info(request):
         buyerId = result.get('user', '')
         tripReference = result.get('tripRef', '')
 
+        print(merchant_trade_no)
         checkMac = genCheckMacValue(merchant_trade_no, transaction_time, trade_amt, buyerId, tripReference)
         print(check_mac_value == checkMac)
         print(checkMac)
-
+        print(check_mac_value)
+        
         if (rtn_code == 1 and check_mac_value == checkMac):
             print('matching')
             # update transaction: status, trade_no, create docs, and ....
