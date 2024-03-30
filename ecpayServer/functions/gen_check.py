b556e370dea76cdc5386baa721550cd9cad70564
heyounihao <97096673+heyounihao@users.noreply.github.com>
2024-03-30 16:26:26 +0800

    new

--- ecpayServer/functions/gen_check.py
+++ ecpayServer/functions/gen_check.py
@@ -6,21 +6,21 @@ def genCheckMacValue(orderId, transactionTime, price, buyerId, tripId):
     hashIV = 'EkRm7iFT261dpevs'  
 
     queryParams = {
-        'MerchantID': '3002607',
-        'MerchantTradeNo': 'ridar135790',
-        'MerchantTradeDate': '2023/12/11 10:23:24',
+        'MerchantID': '2000132',
+        'MerchantTradeNo': orderId,
+        'MerchantTradeDate': transactionTime.strftime("%Y/%m/%d %H:%M:%S"),
         'PaymentType': 'aio',
-        'TotalAmount': 350,
+        'TotalAmount': price,
         'TradeDesc': '訂單測試',
         'ItemName': '旅程',
         'ReturnURL': 'https://ridar-server.vercel.app/return',
         # 'OrderResultURL': 'https://ridar-server.vercel.app/clientResult',
         'ChoosePayment': 'ALL',
-        # 'ClientBackURL': f'https://ridar.com.tw/paymentResult/{orderId}?tripRef={tripId}',
-        # 'Remark': '交易備註',
-        # 'IgnorePayment': 'ATM#CVS#BARCODE#BNPL',
-        # 'CustomField1': buyerId,
-        # 'CustomField2': tripId,
+        'ClientBackURL': f'https://ridar.com.tw/paymentResult/{orderId}?tripRef={tripId}',
+        'Remark': '交易備註',
+        'IgnorePayment': 'ATM#CVS#BARCODE#BNPL',
+        'CustomField1': buyerId,
+        'CustomField2': tripId,
         'EncryptType': '1',
     }
 
