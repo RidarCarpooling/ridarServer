d60b7149391bce1cce0cdc692c562c9b0222cb0d
heyounihao <97096673+heyounihao@users.noreply.github.com>
2024-04-04 17:35:10 +0800

    update

--- ecpayServer/ecpayServer/urls.py
+++ ecpayServer/ecpayServer/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('', include("payment.urls")),
     path('payment/', include("payment.urls")),
     path('return', include("receive_result.urls")),
     path('refund', include('refund.urls')),
+    path('favicon.ico',RedirectView.as_view(url='/static/images/favicon.ico')),
     path('admin/', admin.site.urls)
 ]
--- ecpayServer/refund/views.py
+++ ecpayServer/refund/views.py
@@ -13,61 +13,63 @@ def refund(request):
             current_time.time() <= datetime.strptime('20:30', '%H:%M').time():
         time.sleep((datetime.strptime('20:30', '%H:%M') - current_time.time()).total_seconds())
 
-    orderId = request.POST.get('orderId')
+    orderId = request.POST.get('orderId', [])
     refundType = request.POST.get('refundType')
     print('Call the api successfully', orderId, refundType)
 
-    try:
-        tradeDetails = read_transaction_from_firebase(orderId)
-        creditRefundId = tradeDetails.get('creditRefundId', '')
-        creditAmount = tradeDetails.get('finalPrice', '')
-        tradeNo = tradeDetails.get('tradeNo', '')
-        startTime = tradeDetails.get('startTime', '')
-        moneyViaWallet = tradeDetails.get('moneyViaWallet', '')
-    except Exception as e:
-        print('Transaction data not found', e)
 
-    if creditAmount > 0:
+    for orderNo in orderId:
         try:
-            result = search_single_transaction(creditRefundId, creditAmount)
-            status = result['RtnValue']['status']
+            tradeDetails = read_transaction_from_firebase(orderNo)
+            creditRefundId = tradeDetails.get('creditRefundId', '')
+            creditAmount = tradeDetails.get('finalPrice', '')
+            tradeNo = tradeDetails.get('tradeNo', '')
+            startTime = tradeDetails.get('startTime', '')
+            moneyViaWallet = tradeDetails.get('moneyViaWallet', '')
         except Exception as e:
-            print('An exception occurred while searching transaction:', e)
-            status = ''
+            print('Transaction data not found', e)
 
-        # full refund
-        if refundType == 'full': 
-            if status == '已授權':
-                perform_credit_do_action(orderId, tradeNo, creditAmount, action='N')
-            elif status == '要關帳':
-                perform_credit_do_action(orderId, tradeNo, creditAmount, action='E')
-                perform_credit_do_action(orderId, tradeNo, creditAmount, action='N')
-            elif status == '已關帳':
-                perform_credit_do_action(orderId, tradeNo, creditAmount, action='R')
-            
-            tradeDetails['paymentStatus'] = 'cancelled'
-            tradeDetails['passengerCost'] = 0
-            tradeDetails['driverEarned'] = 0
-            write_transaction_to_firebase(orderId, tradeDetails)
-        
-        # partial refund
-        elif refundType == 'partial':
-            refund_value = calculate_refund_value(startTime, creditAmount, moneyViaWallet)
-            if refund_value:
+        if creditAmount > 0:
+            try:
+                result = search_single_transaction(creditRefundId, creditAmount)
+                status = result['RtnValue']['status']
+            except Exception as e:
+                print('An exception occurred while searching transaction:', e)
+                status = ''
+
+            # full refund
+            if refundType == 'full': 
                 if status == '已授權':
-                    perform_credit_do_action(orderId, tradeNo, creditAmount, action='C')
-                    perform_credit_do_action(orderId, tradeNo, refund_value, action='R')
-                elif status in ['要關帳', '已關帳']:
-                    perform_credit_do_action(orderId, tradeNo, refund_value, action='R')
+                    perform_credit_do_action(orderId, tradeNo, creditAmount, action='N')
+                elif status == '要關帳':
+                    perform_credit_do_action(orderId, tradeNo, creditAmount, action='E')
+                    perform_credit_do_action(orderId, tradeNo, creditAmount, action='N')
+                elif status == '已關帳':
+                    perform_credit_do_action(orderId, tradeNo, creditAmount, action='R')
+                
                 tradeDetails['paymentStatus'] = 'cancelled'
-                tradeDetails['passengerCost'] = (creditAmount + moneyViaWallet) * 0.5
-                tradeDetails['driverEarned'] = (creditAmount + moneyViaWallet) * 0.35
+                tradeDetails['passengerCost'] = 0
+                tradeDetails['driverEarned'] = 0
                 write_transaction_to_firebase(orderId, tradeDetails)
-            # cannot refund
-            else:
-                tradeDetails['driverEarned'] = (creditAmount + moneyViaWallet) * 0.7
-                write_transaction_to_firebase(orderId, tradeDetails)
-    
+            
+            # partial refund
+            elif refundType == 'partial':
+                refund_value = calculate_refund_value(startTime, creditAmount, moneyViaWallet)
+                if refund_value:
+                    if status == '已授權':
+                        perform_credit_do_action(orderId, tradeNo, creditAmount, action='C')
+                        perform_credit_do_action(orderId, tradeNo, refund_value, action='R')
+                    elif status in ['要關帳', '已關帳']:
+                        perform_credit_do_action(orderId, tradeNo, refund_value, action='R')
+                    tradeDetails['paymentStatus'] = 'cancelled'
+                    tradeDetails['passengerCost'] = (creditAmount + moneyViaWallet) * 0.5
+                    tradeDetails['driverEarned'] = (creditAmount + moneyViaWallet) * 0.35
+                    write_transaction_to_firebase(orderId, tradeDetails)
+                # cannot refund
+                else:
+                    tradeDetails['driverEarned'] = (creditAmount + moneyViaWallet) * 0.7
+                    write_transaction_to_firebase(orderId, tradeDetails)
+        
     return HttpResponse('Method not allowed', status=405)
 
 def calculate_refund_value(startTime, credit_amount, money_via_wallet):
