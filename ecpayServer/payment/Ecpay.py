b870db87a477af979888c1005f6f231edb379bb2
heyounihao <97096673+heyounihao@users.noreply.github.com>
2024-04-17 20:19:11 +0800

    update

--- ecpayServer/payment/Ecpay.py
+++ ecpayServer/payment/Ecpay.py
@@ -67,6 +67,7 @@ def main(totalAmount, orderId, transactionTime, buyerId, tripReference, lang):
     #     'Redeem': 'N',
     #     'UnionPay': 0,
     # }
+    print(order_params)
 
     inv_params = {
         # 'RelateNumber': 'Tea0001', # 特店自訂編號
@@ -112,7 +113,7 @@ def main(totalAmount, orderId, transactionTime, buyerId, tripReference, lang):
     try:
         # 產生綠界訂單所需參數
         final_order_params = ecpay_payment_sdk.create_order(order_params)
-        check = ecpay_payment_sdk.generate_check_value(final_order_params)
+        print(final_order_params)
         # 產生 html 的 form 格式
         action_url = 'https://payment-stage.ecpay.com.tw/Cashier/AioCheckOut/V5'  # 測試環境
         # action_url = 'https://payment.ecpay.com.tw/Cashier/AioCheckOut/V5' # 正式環境
