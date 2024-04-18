2fdc84b92a05e782b5bbd4a87e84edea8f4e8e1a
heyounihao <97096673+heyounihao@users.noreply.github.com>
2024-04-18 11:39:34 +0800

    update

--- ecpayServer/refund/views.py
+++ ecpayServer/refund/views.py
@@ -28,7 +28,8 @@ def refund(request):
     print(type(order_ids_str))
     print(order_ids_str[0])
     print(type(order_ids_str[0]))
-    order_ids_list = order_ids_str[0]
+    order_ids_list = json.loads(order_ids_str[0])
+    print(type(order_ids_list))
     # print(order_ids_str)
     # order_ids_str = order_ids_str[0].strip("[]")  # Remove square brackets
     # order_id_list = [order_id.strip() for order_id in order_ids_str.split(",")]  # Split by comma and strip whitespace
